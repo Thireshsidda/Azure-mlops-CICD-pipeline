caf2d22c8a6d5ce3fb7817b2602a380d77be7d7a
Thiresh Sidda <92287626+Thireshsidda@users.noreply.github.com>
2025-01-23 09:26:14 +0530

    Update register.py

--- data-science/src/register.py
+++ data-science/src/register.py
@@ -1,12 +1,11 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 """
-Registers trained ML model if deploy flag is True.
+Registers the best-trained ML model from the sweep job.
 """
 
 import argparse
 from pathlib import Path
-import pickle
 import mlflow
 import os 
 import json
@@ -17,7 +16,6 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', type=str, help='Name under which model will be registered')
     parser.add_argument('--model_path', type=str, help='Model directory')
-    parser.add_argument('--evaluation_output', type=str, help='Path of eval results')
     parser.add_argument("--model_info_output_path", type=str, help="Path to write model info JSON")
     args, _ = parser.parse_known_args()
     print(f'Arguments: {args}')
@@ -25,39 +23,31 @@ def parse_args():
     return args
 
 def main(args):
-    '''Loads model, registers it if deploy flag is True'''
+    '''Loads the best-trained model from the sweep job and registers it'''
 
-    with open((Path(args.evaluation_output) / "deploy_flag"), 'rb') as infile:
-        deploy_flag = int(infile.read())
-        
-    mlflow.log_metric("deploy flag", int(deploy_flag))
+    print("Registering ", args.model_name)
 
-    if deploy_flag == 1:
-        print("Registering ", args.model_name)
+    # Load model
+    model = mlflow.sklearn.load_model(args.model_path)
 
-        # Load model
-        model = mlflow.sklearn.load_model(args.model_path)
+    # Log model using mlflow
+    mlflow.sklearn.log_model(model, args.model_name)
 
-        # Log model using mlflow
-        mlflow.sklearn.log_model(model, args.model_name)
+    # Register logged model using mlflow
+    run_id = mlflow.active_run().info.run_id
+    model_uri = f'runs:/{run_id}/{args.model_name}'
+    mlflow_model = mlflow.register_model(model_uri, args.model_name)
+    model_version = mlflow_model.version
 
-        # Register logged model using mlflow
-        run_id = mlflow.active_run().info.run_id
-        model_uri = f'runs:/{run_id}/{args.model_name}'
-        mlflow_model = mlflow.register_model(model_uri, args.model_name)
-        model_version = mlflow_model.version
-
-        # Write model info
-        print("Writing JSON")
-        model_info = {"id": f"{args.model_name}:{model_version}"}
-        output_path = os.path.join(args.model_info_output_path, "model_info.json")
-        with open(output_path, "w") as of:
-            json.dump(model_info, of)
-    else:
-        print("Model will not be registered!")
+    # Write model info
+    print("Writing JSON")
+    model_info = {"id": f"{args.model_name}:{model_version}"}
+    output_path = os.path.join(args.model_info_output_path, "model_info.json")
+    with open(output_path, "w") as of:
+        json.dump(model_info, of)
 
 if __name__ == "__main__":
-
+    
     mlflow.start_run()
     
     # Parse Arguments
@@ -66,7 +56,7 @@ if __name__ == "__main__":
     lines = [
         f"Model name: {args.model_name}",
         f"Model path: {args.model_path}",
-        f"Evaluation output path: {args.evaluation_output}",
+        f"Model info output path: {args.model_info_output_path}"
     ]
 
     for line in lines:
